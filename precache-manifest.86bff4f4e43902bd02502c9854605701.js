4cb0122bd787e885db915c4a1476ec208e0f0b51
Suman <sumansikhamanjhi@gmail.com>
2020-05-04 12:19:50 +0530

    Updates

---
+++ precache-manifest.86bff4f4e43902bd02502c9854605701.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a69a1bed710e7a18c46199d728e1a391",
+    "url": "/Calculator/index.html"
+  },
+  {
+    "revision": "cbb20b39ba476733155e",
+    "url": "/Calculator/static/css/main.b3644f49.chunk.css"
+  },
+  {
+    "revision": "e296a77957d697b7a93d",
+    "url": "/Calculator/static/js/2.5250392c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Calculator/static/js/2.5250392c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cbb20b39ba476733155e",
+    "url": "/Calculator/static/js/main.8f9fb4e6.chunk.js"
+  },
+  {
+    "revision": "ecedcecfa5e69224ee2c",
+    "url": "/Calculator/static/js/runtime-main.2da71ecd.js"
+  }
+]);
